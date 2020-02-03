2f6fcd0234f501adc5bfd583866d7e3fed0e4181
Bogdan Panchuk <bogdan.panchuck@intapp.com>
2020-02-03 23:59:26 +0200

    Added Boolan search. Added Incidence Matrix index in addition to Inverted Index.

--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	code.sajari.com/docconv v1.1.0
 	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
 	github.com/advancedlogic/GoOse v0.0.0-20191112112754-e742535969c1 // indirect
-	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/deckarep/golang-set v1.7.1
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/jdkato/prose v1.1.1
 	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
 	github.com/mingrammer/commonregex v1.0.1 // indirect
+	github.com/willf/bitset v1.1.10
 	gonum.org/v1/gonum v0.6.2 // indirect
 	gopkg.in/jdkato/prose.v2 v2.0.0-20190814032740-822d591a158c
 	gopkg.in/neurosnap/sentences.v1 v1.0.6 // indirect
--- main.go
+++ main.go
@@ -9,13 +9,18 @@ import (
 	"strings"
 
 	"code.sajari.com/docconv"
+	mapset "github.com/deckarep/golang-set"
+	bitset "github.com/willf/bitset"
 	"gopkg.in/jdkato/prose.v2"
 )
 
 var punctuation = []string{".", "!", "?", ":", ";", ",", "(", ")", "—", "·"}
 
-// InvIndex - mapping from word to all the document names it occurs in
-type InvIndex map[string]StringSet
+// InvIndex - mapping from tokens to all the document names it occurs in
+type InvIndex map[string]mapset.Set
+
+// IncMatrix - incidence matrix of tokens and documents
+type IncMatrix map[string]bitset.BitSet
 
 // IsIn - checks if the value is in a slice
 func IsIn(slice []string, val string) bool {
@@ -28,7 +33,7 @@ func IsIn(slice []string, val string) bool {
 }
 
 // CreateInvIndex - Create inverse index of words for text files in a directory
-func CreateInvIndex(dir string) InvIndex {
+func CreateInvIndex(dir string) (InvIndex, mapset.Set) {
 	invIndex := make(InvIndex)
 
 	files, err := filepath.Glob(dir + "/*") // ioutil.ReadDir("./texts/")
@@ -38,8 +43,10 @@ func CreateInvIndex(dir string) InvIndex {
 
 	log.Printf("Indexing %v documents.", len(files))
 
+	fullSet := mapset.NewSet()
 	for _, file := range files {
-		// os.Stats(f)
+		fullSet.Add(file)
+
 		res, err := docconv.ConvertPath(file)
 		if err != nil {
 			log.Fatal(err)
@@ -55,7 +62,7 @@ func CreateInvIndex(dir string) InvIndex {
 		for _, token := range doc.Tokens() {
 			if !IsIn(punctuation, token.Text) {
 				if _, ok := invIndex[token.Text]; !ok {
-					invIndex[token.Text] = make(StringSet)
+					invIndex[token.Text] = mapset.NewSet()
 				}
 
 				invIndex[token.Text].Add(file)
@@ -63,7 +70,46 @@ func CreateInvIndex(dir string) InvIndex {
 		}
 	}
 
-	return invIndex
+	return invIndex, fullSet
+}
+
+// CreateIncMatrix - Create incidence matrix of words for text files in a directory
+func CreateIncMatrix(dir string) (IncMatrix, []string) {
+	files, err := filepath.Glob(dir + "/*") // ioutil.ReadDir("./texts/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Indexing %v documents.", len(files))
+
+	docNames := make([]string, len(files))
+	incMatrix := make(IncMatrix)
+
+	for i, file := range files {
+		docNames[i] = file
+
+		// os.Stats(f)
+		res, err := docconv.ConvertPath(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doc, err := prose.NewDocument(res.Body, prose.WithExtraction(false), prose.WithTagging(false))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("%v: Found %v tokens.", file, len(doc.Tokens()))
+
+		for _, token := range doc.Tokens() {
+			if !IsIn(punctuation, token.Text) {
+				vec := incMatrix[token.Text]
+				incMatrix[token.Text] = *vec.Set(uint(i))
+			}
+		}
+	}
+
+	return incMatrix, docNames
 }
 
 // Count - returns the number of entries in InvIndex
@@ -72,10 +118,23 @@ func (invIndex InvIndex) Count() int { return len(invIndex) }
 // Find - searches for the given token in an inverse index
 func (invIndex InvIndex) Find(token string) []string {
 	index := invIndex[token]
-	entries := make([]string, len(index))
+	entries := make([]string, index.Cardinality())
 	i := 0
-	for k := range index {
-		entries[i] = k
+	for doc := range index.Iter() {
+		entries[i] = doc.(string)
+		i++
+	}
+
+	return entries
+}
+
+// Find - searches for the given token in an inverse index
+func (incMatrix IncMatrix) Find(token string, docNames []string) []string {
+	vec := incMatrix[token]
+	entries := make([]string, vec.Count())
+	i := 0
+	for bit, set := vec.NextSet(0); set; bit, set = vec.NextSet(bit + 1) {
+		entries[i] = docNames[bit]
 		i++
 	}
 
@@ -115,22 +174,112 @@ func Load(file string) InvIndex {
 	return invIndex
 }
 
+// Vals - converts token to incidence bitset
+func (incMatrix IncMatrix) Vals(t string) (result bitset.BitSet) {
+	if set, ok := incMatrix[t]; ok {
+		return set
+	}
+
+	return
+}
+
+// And - logical AND
+func And(t1 bitset.BitSet, t2 bitset.BitSet) bitset.BitSet {
+	return *t1.Intersection(&t2)
+}
+
+// Or - logical OR
+func Or(t1 bitset.BitSet, t2 bitset.BitSet) bitset.BitSet {
+	return *t1.Union(&t2)
+}
+
+// Not - logical NOT
+func Not(t bitset.BitSet) bitset.BitSet {
+	return *t.Complement()
+}
+
+// IncToDocuments - translates incidence bitset to document names
+func IncToDocuments(vec bitset.BitSet, docNames []string) []string {
+	entries := make([]string, vec.Count())
+	i := 0
+	for bit, set := vec.NextSet(0); set; bit, set = vec.NextSet(bit + 1) {
+		entries[i] = docNames[bit]
+		i++
+	}
+
+	return entries
+}
+
+// IndToDocuments - translates index to document names
+func IndToDocuments(ind mapset.Set) []string {
+	entries := make([]string, ind.Cardinality())
+	i := 0
+	for doc := range ind.Iter() {
+		entries[i] = doc.(string)
+		i++
+	}
+
+	return entries
+}
+
+// Vals - converts token to inverted index
+func (invIndex InvIndex) Vals(t string) (result mapset.Set) {
+	if set, ok := invIndex[t]; ok {
+		return set
+	}
+
+	return
+}
+
+// And2 - logical AND
+func And2(t1 mapset.Set, t2 mapset.Set) mapset.Set {
+	return t1.Intersect(t2)
+}
+
+// Or2 - logical OR
+func Or2(t1 mapset.Set, t2 mapset.Set) mapset.Set {
+	return t1.Union(t2)
+}
+
+// Not2 - logical NOT
+func Not2(t mapset.Set, full mapset.Set) mapset.Set {
+	return full.Difference(t)
+}
+
 func main() {
-	invIndexFile := "inv-index.gob"
+	// invIndexFile := "inv-index.gob"
+	// CreateInvIndex("./texts").Save(invIndexFile)
+	// invIndex := Load(invIndexFile)
 
-	CreateInvIndex("./texts").Save(invIndexFile)
-
-	invIndex := Load(invIndexFile)
+	invIndex, fullSet := CreateInvIndex("./texts")
+	incMatrix, docNames := CreateIncMatrix("./texts")
 
 	fmt.Println("Unique tokens count:", invIndex.Count())
 
 	// ==================================
 
 	token := "input"
+
 	entries := invIndex.Find(token)
 	fmt.Println(token, ":", strings.Join(entries, ", "))
 
-	token = "был"
+	token = "users"
+
 	entries = invIndex.Find(token)
 	fmt.Println(token, ":", strings.Join(entries, ", "))
+
+	token = "был"
+
+	entries = invIndex.Find(token)
+	fmt.Println(token, ":", strings.Join(entries, ", "))
+
+	// Incidence Matrix
+	inc := Or(incMatrix.Vals("был"), Not(And(incMatrix.Vals("input"), incMatrix.Vals("users"))))
+	entries = IncToDocuments(inc, docNames)
+	fmt.Println("был OR NOT (input AND users):", strings.Join(entries, ", "))
+
+	// Inverted Index
+	ind := Or2(invIndex.Vals("был"), Not2(And2(invIndex.Vals("input"), invIndex.Vals("users")), fullSet))
+	entries = IndToDocuments(ind)
+	fmt.Println("был OR NOT (input AND users):", strings.Join(entries, ", "))
 }
